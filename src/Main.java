b4ee7241bedf74e979936b332a64ad10779542d5
Link <bal2457@utexas.edu>
2021-12-03 19:40:50 -0600

    Added starter project files
    Created java project and added basic pseudocode

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Open file
+		//Import data from file
+		//Process data into data structure
+		
+		//Calculate the total average number of queries that each server (data1 and data2 combined) received
+		
+		//Calculate the average number of queries that were received on data1 and data2 for all instances
+		
+		//Print the number of spikes each instance (both data1 and data2 inclusive) saw. A spike is defined as any value of 700 and up
+		
+		//Profile your code. Add timestamps between each step 1 to 4 above and print the time(in any unit you want) it took to output the result
+
+	}
+
+}
