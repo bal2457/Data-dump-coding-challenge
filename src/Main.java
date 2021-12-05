5bf6bee441e5f3ce3e251325789444e97c6873f8
Link <bal2457@utexas.edu>
2021-12-05 17:09:54 -0600

    Fixed JSON Iterator and finished additional parts
    Fixed JSON Iterator and implemented functionality and properly formatted print displays for parts 2, 3, and 4

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -14,12 +14,11 @@ import org.json.simple.parser.*;
 public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
-		//Open file
-		//Import data from file
-		//Process data into data structure
 		
-		//JSON Data structure, uses Server instance as key, and data network/time/value in wrapper function as value
-		Map<String, Wrapper> JSONData = new HashMap<>(); 
+		//1. Ingest attached data from a file
+		
+		//JSON Data arraylist with instance/network/timestamp/value in wrapper class
+		ArrayList<JSONWrapper> JSONData = new ArrayList<JSONWrapper>();
 		
 		//Creates JSON Array structure to iterate over JSON
 		JSONParser jsonParser = new JSONParser();
@@ -33,59 +32,192 @@ public class Main {
 		String timestamp = "timestamp";
 		String value = "value";
 		
-		//Iterates over JSON and adds all values to HashMap structure
+		
+		//Iterates over JSON and adds all values to ArrayList structure
 		for(int index = 0; index < jsonArray.size(); index++) {
-			//System.out.println(jsonArray.get(index));
-			Object j = jsonArray.remove(index);
+			
+			Object j = jsonArray.get(index);
 			JSONObject jobj = (JSONObject) j;
 			if(jobj.get(value) != null) {
-				JSONData.put((String)jobj.get(ip), new Wrapper((String)jobj.get(network), OffsetDateTime.parse((CharSequence) jobj.get(timestamp)) , Integer.parseInt((String) jobj.get(value))));
-			}else {
-				JSONData.put((String)jobj.get(ip), new Wrapper((String)jobj.get(network), OffsetDateTime.parse((CharSequence) jobj.get(timestamp)) , 0));
+				JSONData.add(new JSONWrapper((String)jobj.get(network), (String)jobj.get(ip), OffsetDateTime.parse((CharSequence) jobj.get(timestamp)) , Integer.parseInt((String) jobj.get(value))));	
 			}
+			else {
+				JSONData.add(new JSONWrapper((String)jobj.get(network), (String)jobj.get(ip), OffsetDateTime.parse((CharSequence) jobj.get(timestamp)) , 0));
+			}	
+		}
+
+		
+		
+		
+		
+		
+		
+		
+		
+		//2. Calculate the total average number of queries that each server (data1 and data2 combined) received
+		
+		//Data structure that holds server instance as key and wrapper class for queries as value
+		HashMap<String, ServerQueryWrapper> averageServerQueryMap = new HashMap<String, ServerQueryWrapper>();
+		
+		for(JSONWrapper w: JSONData){
+			String serverInstance = w.getInstance();
 			
-			
+			//if hashmap contains server instance
+			if(averageServerQueryMap.containsKey(serverInstance)) {
+		
+				averageServerQueryMap.get(serverInstance).incrementTotalQueries(w.getValue());
+				averageServerQueryMap.get(serverInstance).incrementTotalTimeStamps(); 
+			}
+			else {
+				//else create new server instance in hashmap
+				averageServerQueryMap.put(serverInstance, new ServerQueryWrapper(w.getValue(),1));
+			}
+		}
+		
+		//Format and printing for instance and average queries
+		System.out.println("Instance \t\t Average");
+		for(String s:averageServerQueryMap.keySet()) {
+			System.out.print(s);
+			System.out.format("\t %d%n",averageServerQueryMap.get(s).getTotalQueries()/averageServerQueryMap.get(s).getTotalTimeStamps());
 		}
 		
 		
 		
 		
-		//Calculate the total average number of queries that each server (data1 and data2 combined) received
 		
-		//Calculate the average number of queries that were received on data1 and data2 for all instances
 		
-		//Print the number of spikes each instance (both data1 and data2 inclusive) saw. A spike is defined as any value of 700 and up
+		
+		
+		
+		
+		
+		
+		//3. Calculate the average number of queries that were received on data1 and data2 for all instances
+		
+		//Data structure that holds interface as key and wrapper class for queries as value
+		HashMap<String, ServerQueryWrapper> averageDataQueryMap = new HashMap<String, ServerQueryWrapper>();
+		
+		for(JSONWrapper w: JSONData) {
+			String dataInstance = w.getDataNetwork();
+			
+			if(averageDataQueryMap.containsKey(dataInstance)){
+				averageDataQueryMap.get(dataInstance).incrementTotalQueries(w.getValue());
+				averageDataQueryMap.get(dataInstance).incrementTotalTimeStamps();
+			}
+			else {
+				averageDataQueryMap.put(dataInstance,new ServerQueryWrapper(w.getValue(),1));
+			}
+		}
+		
+		//Format and printing for interface and average queries
+		System.out.println("\nInterface \t\t Average");
+		for(String s:averageDataQueryMap.keySet()) {
+			System.out.print(s);
+			System.out.format("\t\t\t %d%n",averageDataQueryMap.get(s).getTotalQueries()/averageDataQueryMap.get(s).getTotalTimeStamps());
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//4.Print the number of spikes each instance (both data1 and data2 inclusive) saw. A spike is defined as any value of 700 and up
+		
+		//Data structure that holds server instance as key and total number of spikes as value
+		HashMap<String, Integer> serverSpikeMap = new HashMap<String, Integer>();
+		
+		for(JSONWrapper w: JSONData){
+			String serverInstance = w.getInstance();
+			
+			//if server instance is already in keyset
+			if(serverSpikeMap.containsKey(serverInstance)) {
+				if(isSpike(w.getValue())){
+					int currentSpikes = serverSpikeMap.get(serverInstance)+1;
+					serverSpikeMap.put(serverInstance, currentSpikes);
+		
+				}	
+			}
+			
+			//if server instance is not in keyset, then add it
+			else {
+				if(isSpike(w.getValue())){
+					serverSpikeMap.put(serverInstance, 1);
+				}
+				else {
+					serverSpikeMap.put(serverInstance, 0);
+				}
+			}
+
+		}
+		//Format and printing for instance and average queries
+		System.out.println("\nInstance \t\t Number of spikes");
+		for(String s:serverSpikeMap.keySet()) {
+			System.out.print(s);
+			System.out.format("\t %d%n",serverSpikeMap.get(s));
+		}
+		
+		
+		
+		
+		
+		
 		
 		//Profile your code. Add timestamps between each step 1 to 4 above and print the time(in any unit you want) it took to output the result
 
+	}
+	
+	//helper method for determining if queries are a spike, assumes a spike is at least 700 queries
+	static boolean isSpike(int spike) {
+		int spikeValue = 700;
+		if(spike >= spikeValue)return true;
+		else return false;
 	}
 
 }
 
 //Wrapper class for holding JSON Data
-//Assumes data has data network stored as string, time set in ISO 8601 format, and value stored as integer
-class Wrapper{
+//Assumes data has data network and server instance stored as string, time set in ISO 8601 format and stored as an OffsetDateTime, and value stored as integer
+class JSONWrapper{
 	
 	private String dataNetwork;
+	private String instance;
 	private OffsetDateTime time;
 	private int value;
 	
-	public Wrapper(String dataNetwork, OffsetDateTime time, int value) {
+	public JSONWrapper(String dataNetwork, String instance, OffsetDateTime time, int value) {
 		this.dataNetwork = dataNetwork;
+		this.instance = instance;
 		this.time = time;
 		this.value = value;
 	}
 	
 	public String getDataNetwork(){return this.dataNetwork;}
+	public String getInstance(){return this.instance;}
 	public OffsetDateTime getTime(){return this.time;}
 	public int getValue(){return this.value;}
+	
+}
+
+class ServerQueryWrapper{
+	
+	private int totalQueries;
+	private int totalTimeStamps;
+	
+	public ServerQueryWrapper(int totalQueries, int totalTimeStamps) {
+		this.totalQueries = totalQueries;
+		this.totalTimeStamps = totalTimeStamps;
+	}
+	
+	public int getTotalQueries() {return this.totalQueries;}
+	public int getTotalTimeStamps(){return this.totalTimeStamps;}
+	
+	public void incrementTotalQueries(int additionalQueries) {this.totalQueries+= additionalQueries;}
+	public void incrementTotalTimeStamps() {this.totalTimeStamps++;}
 	
 	
 }
 
-/*for(Wrapper w: JSONData.values()) {
-System.out.println(w.getDataNetwork());
-System.out.println(w.getTime());
-System.out.println(w.getValue());
-
-}*/
